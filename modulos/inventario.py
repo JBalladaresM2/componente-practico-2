83012e9e03bb1b4b9ef114dbfcc32e2aa3d64f6d
Jonabravo34 <jonavo574@gmail.com>
2026-07-05 16:20:41 -0500

    parte1

--- modulos/inventario.py
+++ modulos/inventario.py
@@ -1,15 +1,20 @@
-medicamentos = open("datos/medicamentos.txt" , "r")
-inventario = [
-]
-lista=[]
-for linea in medicamentos:
-    medicamento=linea.strip().split(",")
-    print(medicamento)
-    nombre=medicamento[0]
-    cantidad=int(medicamento[1])
-    lista.append((nombre , cantidad))
-print(lista)
+def inventario() :
+ medicamentos = open("datos/medicamentos.txt" , "r") 
+ lista=[]
+ for linea in medicamentos:
+        medicamento=linea.strip().split(",")
+        nombre=medicamento[0]
+        cantidad=int(medicamento[1])
+        precio=float(medicamento[2])
+        lista.append((nombre , cantidad , precio))
+        medicamentos.close()
+ return lista 
+def cambio(lista_nueva):
+ medicamentos = open("datos/medicamentos.txt" , "w")
+ for linea in medicamentos:
+    nombre, cantidad, precio = linea
+    linea=f"{nombre},{cantidad},{precio}\n"
+    medicamentos.write(linea)
+    return(print("Inventario guardado con exito"))
 
 
-
- 
